964419ae84cd29839b7eb82935c15dbfa1120a25
JeremyB <jbaldner3@yahoo.com>
2024-02-04 20:15:16 -0600

    Modified app_server to get data travel from API. Request installed.

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,11 +1,50 @@
-const fs = require('fs');
-const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+const { response } = require('express');
+const request = require('request');
+const apiOptions = { server: 'http://localhost:3000' }
 
-const travel = (req, res) => {
-    pageTitle = 'Travlr Getaways - Travel';
-    res.render('travel', {title: pageTitle, trips, highlight_travel: true});
+// render travel list
+const renderTravelList = (req, res, responseBody) => {
+    let message = null;
+    let pageTitle = process.env.npm_package_description + ' - travel';
+
+    if (!(responseBody instanceof Array)) {
+        message = 'API lookup error';
+        responseBody = [];
+    } else {
+        if (!responseBody.length) {
+            message = 'No trips exist in databse!';
+        }
+    }
+
+    res.render('travel', {
+        title: pageTitle,
+        trips: responseBody,
+        message
+    });
 };
 
-module.exports = {
-    travel
+// GET travel information
+const travelList = (req, res) => {
+    const path = '/api/trips';
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: 'GET',
+        json: {}
+    };
+
+    console.info('>> travelController.travelList calling ' + requestOptions.url);
+
+    request(
+        requestOptions,
+        (err, {statusCode}, body) => {
+            if (err) {
+                console.error(err);
+            }
+            renderTravelList(req, res, body);
+        }
+    )
 }
+
+module.exports = {
+    travelList
+};
